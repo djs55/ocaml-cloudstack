7e3bf6225dd224b561a48ee28869665ee30384a1
David Scott <dave.scott@citrix.com>
2014-04-15 22:03:18 +0100

    generator: deduplicate parameters
    ListVolumes introspection reports a duplicate 'storageid' parameter.
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- generator/src/generator.ml
+++ generator/src/generator.ml
@@ -1,6 +1,13 @@
 open ListApis_t
 open Utils
 
+let dedup_assoc xs =
+  let rec loop names acc = function
+  | [] -> List.rev acc
+  | x :: xs ->
+    loop (x.name :: names) (if List.mem x.name names then acc else x :: acc) xs in
+  loop [] [] xs
+
 let _ =
   let json = ref "" in
   Arg.parse [
@@ -14,6 +21,16 @@ let _ =
   end;
   let text = string_of_file !json in
   let t = ListApis_j.t_of_string text in
+
+  (* ListVolumes has duplicate 'storageid' parameter *)
+  let t =
+    let api = List.map
+      (fun api ->
+         let params = dedup_assoc api.params in
+         { api with params }
+      ) t.listapisresponse.api in
+    { listapisresponse = { t.listapisresponse with api }} in
+
   (* TODO: consider grouping the APIs the same way they appear in the online
    * docs *)
   (* Generate API bindings *)
